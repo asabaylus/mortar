0e00af81146c9b782b5d29c15a3f982cc318c0a8
Welch Canavan <welch.canavan@gmail.com>
2016-03-01 09:35:57 -0500

    add scripting tasks, conventions, and modal module

--- app/modules/modals/scripts.js
+++ app/modules/modals/scripts.js
@@ -1,1 +1,34 @@
-module.exports = console.log( "Hello, world!" );
+const $ = require('jquery');
+
+const modules = {
+  init: function() {
+    const _this = this;
+
+    $('body').click(function(e){
+      if (e.target.className === 'mt_JS_modal-open-button') {
+        return _this.open(e.target.dataset.modalTarget)
+      } else if (e.target.className === 'mt_JS_modal-close-button') {
+        return _this.close(e.target.dataset.modalTarget)
+      }
+    });
+  },
+  close: function(id) {
+    const targetModal = $('.mt_JS_modal-container[data-modal-id="' + id + '"]');
+    const transitionDuration = parseFloat(targetModal.css('transition-duration')) * 1000;
+
+    targetModal
+    .removeClass('mt_module-container--active');
+
+    // Wait until the transition is done to add the `visuallyhidden` class
+    setTimeout(function(){
+      targetModal.addClass('mt_visuallyhidden')
+    }, transitionDuration);
+  },
+  open: function(id) {
+    $('.mt_JS_modal-container[data-modal-id="' + id + '"]')
+    .removeClass('mt_visuallyhidden')
+    .addClass('mt_module-container--active');
+  }
+};
+
+module.exports = modules.init();
--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,13 +1,16 @@
 var gulp  = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
-// var sourcemaps = require('gulp-sourcemaps');
-// var buffer = require('vinyl-buffer');
-// var babel = require('babelify');
+var buffer = require('vinyl-buffer');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('scripts', function(){
   browserify('./app/scripts/main.js')
+    .transform("babelify", {presets: "es2015"})
     .bundle()
     .pipe(source('main.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./.tmp/assets/scripts'))
 });
