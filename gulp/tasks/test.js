02431d839ebfcec3e50c6b2ee88fd225fe00d613
Welch Canavan <welch.canavan@gmail.com>
2016-03-02 13:09:12 -0500

    add eslint task and rules

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -2,10 +2,23 @@
 
 var gulp = require('gulp');
 var sassLint = require('gulp-sass-lint')
+var eslint = require('gulp-eslint');
 
-gulp.task('test', function(){
-  return gulp.src('app/styles/**/*.scss')
+gulp.task('test', [
+  'eslint',
+  'sasslint'
+]);
+
+gulp.task('eslint', function(){
+  return gulp.src('app/{scripts,modules}/**/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('sasslint', function(){
+  return gulp.src('app/{styles,modules}/**/*.scss')
     .pipe(sassLint())
     .pipe(sassLint.format())
-    .pipe(sassLint.failOnError())
+    .pipe(sassLint.failOnError());
 });
