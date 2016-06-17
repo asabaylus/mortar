ec389f11f3bcdb0b50ce0a31e1654d3d1e349d1c
Welch Canavan <welch.canavan@gmail.com>
2016-06-17 12:59:43 -0400

    fix eslint errors

--- app/scripts/pestle/examples/example1/main.js
+++ app/scripts/pestle/examples/example1/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 import {Pestle} from '../../main.js';
 import MyModule from './MyModule';
 
--- app/scripts/pestle/examples/example3/main.js
+++ app/scripts/pestle/examples/example3/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 import {Pestle} from '../../main.js';
 import MyModule from './MyModule';
 
@@ -5,9 +7,9 @@ Pestle.ModuleManager.register('MyModule', MyModule);
 
 Pestle.init(function(failingInstances, workingInstances) {
   if(failingInstances) {
-    console.err("Next instances failed when initializing", failingInstances);
+    console.err('Next instances failed when initializing', failingInstances);
   }
 
-  console.log("Next instances are working correctly", workingInstances);
+  console.log('Next instances are working correctly', workingInstances);
 
 });
