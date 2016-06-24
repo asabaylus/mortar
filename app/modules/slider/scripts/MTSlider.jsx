b14d6a722a7d3755b996a6166b17828da1df7c84
Welch Canavan <welch.canavan@gmail.com>
2016-06-24 12:31:44 -0400

    add ability to toggle looping/infinte feature, inactive slider button state

--- app/modules/slider/scripts/MTSlider.jsx
+++ app/modules/slider/scripts/MTSlider.jsx
@@ -1,13 +1,19 @@
 'use strict';
 
 import React, { Component, PropTypes }  from 'react';
-import Slick from 'react-slick';
 import {Pestle} from '../../../scripts/pestle/main.js';
+import Slick from 'react-slick';
 import events from './events';
+import classNames from 'classnames';
 
 class PrevButton extends React.Component {
   render() {
-    return <button {...this.props} className='mt_slider-button--prev'>
+    let btnClasses = classNames({
+      'mt_slider-button--prev': true,
+      'mt_slider-button--inactive': !this.props.infinite && this.props.currentSlide === 0
+    });
+
+    return <button {...this.props} className={btnClasses}>
       <span className='mt_visuallyhidden'>Previous</span>
       <svg className='mt_icon'>
         <use xmlnsXlink='http://www.w3.org/1999/xlink' xlinkHref='#chevron-left'></use>
@@ -18,7 +24,12 @@ class PrevButton extends React.Component {
 
 class NextButton extends React.Component {
   render() {
-    return <button {...this.props} className='mt_slider-button--next'>
+    let btnClasses = classNames({
+      'mt_slider-button--next': true,
+      'mt_slider-button--inactive': !this.props.infinite && this.props.currentSlide === this.props.slideCount - 1
+    });
+
+    return <button {...this.props} className={btnClasses}>
       <span className='mt_visuallyhidden'>Next</span>
       <svg className='mt_icon'>
         <use xmlnsXlink='http://www.w3.org/1999/xlink' xlinkHref='#chevron-right'></use>
@@ -37,12 +48,21 @@ class MTSlider extends Component {
     }
   }
 
-  onChangeSlide(currentSlide) {
+  onSlideChange(currentSlide) {
     const slideData = {
       currentSlideIndex: currentSlide
     };
 
     Pestle.PubSub.publish(events.slideChange, slideData);
+  }
+
+  constructor(props) {
+    super(props);
+    this.onSlideChange = this.onSlideChange.bind(this);
+
+    this.state = {
+      currentSlide: 0
+    };
   }
 
   render() {
@@ -51,7 +71,7 @@ class MTSlider extends Component {
       className: 'mt_slider-container mt_intratio--photo mt_bgcolor-neutral-xxd',
       nextArrow: <NextButton />,
       prevArrow: <PrevButton />,
-      useCSS: this.props.useCSS
+      useCSS: this.props.animations
     };
 
     const slides = this.props.slides.map((slide, i) => {
@@ -70,10 +90,13 @@ class MTSlider extends Component {
 }
 
 MTSlider.defaultProps = {
-  useCSS: true
+  animations: true,
+  infinite: true
 }
 
 MTSlider.propTypes = {
+  animations: PropTypes.bool,
+  infinite: PropTypes.bool,
   slides: PropTypes.arrayOf(PropTypes.shape({
     type: PropTypes.string.isRequired
   }))
