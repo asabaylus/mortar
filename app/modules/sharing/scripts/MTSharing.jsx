9520cc4dac11683f0b5115486a7bf8aaae0c1a78
Welch Canavan <welch.canavan@gmail.com>
2016-07-05 15:40:31 -0400

    make composable <SocialButton />, add google +

--- app/modules/sharing/scripts/MTSharing.jsx
+++ app/modules/sharing/scripts/MTSharing.jsx
@@ -3,62 +3,62 @@
 import React, { Component, PropTypes }  from 'react';
 import {Pestle} from '@natgeo/mortar-pestle';
 
-function openShareWindow(url, strWindowName, windowWidth, windowHeight) {
-  var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
-      viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
-      windowLeft = (viewportWidth - windowWidth) / 2,
-      windowTop = (viewportHeight - windowHeight) / 2;
-
-  window.open(
-    url,
-    strWindowName,
-    "status = 1, width = " + windowWidth
-      + ", height = " + windowHeight
-      + ", left = " + windowLeft
-      + ", top = " + windowTop
-      + ", resizable = 0"
-    );
-}
-
-class MTTwitter extends Component {
+class MTSocialButton extends Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
   }
 
+  openShareWindow(site, url, title) {
+    const siteAttr = {
+      facebook: {
+        h: 279,
+        url: "http://www.facebook.com/share.php?u=" + url,
+        w: 575,
+        windowTitle: 'Facebook'
+      },
+      'google-plus': {
+        h: 647,
+        url: "https://plus.google.com/share?url=" + url,
+        w: 500,
+        windowTitle: 'Google Plus'
+      },
+      twitter: {
+        h: 420,
+        url: "https://twitter.com/intent/tweet?url="
+              + url
+              + "&text="
+              + title.replace(/[^a-zA-Z ]/g, "").split(' ').join('+')
+              + "&via=NatGeo",
+        w: 550,
+        windowTitle: 'Twitter'
+      }
+    }
+    const viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    const viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    const windowLeft = (viewportWidth - siteAttr[site].w) / 2;
+    const windowTop = (viewportHeight - siteAttr[site].h) / 2;
+
+    window.open(
+      siteAttr[site].url,
+      siteAttr[site].windowTitle,
+      "status = 1, width = " + siteAttr[site].w
+        + ", height = " + siteAttr[site].h
+        + ", left = " + windowLeft
+        + ", top = " + windowTop
+        + ", resizable = 0"
+      );
+  }
+
   handleClick() {
-    const title = this.props.title.replace(/[^a-zA-Z ]/g, "").split(' ').join('+');
-    const url = "https://twitter.com/intent/tweet?url=" + this.props.url + "&text=" + title + "&via=NatGeo";
-    openShareWindow(url, "Twitter", 550, 420);
+    this.openShareWindow(this.props.site, this.props.url, this.props.title);
   }
 
   render() {
     return (
       <button onClick={this.handleClick} className="mt_btn">
-        <svg className="mt_icon mt_color--social--twitter mt_socialsharing">
-          <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="#social-twitter"></use>
-        </svg>
-      </button>
-    )
-  }
-}
-
-class MTFacebook extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    const url = "https://www.facebook.com/share.php?u=" + this.props.url;
-    openShareWindow(url, "Facebook", 575, 279);
-  }
-
-  render() {
-    return (
-      <button onClick={this.handleClick} className="mt_btn">
-        <svg className="mt_icon mt_color--social--facebook mt_socialsharing">
-          <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="#social-facebook-f"></use>
+        <svg className={"mt_icon mt_socialsharing mt_color--social--" + this.props.site}>
+          <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref={"#social-" + this.props.site}></use>
         </svg>
       </button>
     )
@@ -67,22 +67,38 @@ class MTFacebook extends Component {
 
 class MTSharing extends Component {
   render() {
-    return (
-      <div>
-        <MTTwitter
+    let socialButtons = this.props.socialSites.map(site => {
+      return (
+        <MTSocialButton
+          key={site}
           url={this.props.url}
           title={this.props.title}
+          site={site}
         />
-        <MTFacebook
-          url={this.props.url}
-        />
+      )
+    })
+
+    return (
+      <div>
+        {socialButtons}
       </div>
     );
   }
 }
 
+const socialSites = [
+  'facebook',
+  'twitter',
+  'google-plus'
+]
+
+MTSharing.defaultProps = {
+  socialSites: socialSites
+}
+
 MTSharing.propTypes = {
   url: PropTypes.string,
+  socialSites: PropTypes.arrayOf(PropTypes.oneOf(socialSites)),
   title: PropTypes.string
 }
 
