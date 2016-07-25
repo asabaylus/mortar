13efb5b362d4756dbe7d9a1f610f442f20f79814
Ben Pearson <bpearson@groupcse.com>
2016-07-25 17:21:01 -0400

    [MORTAR-263] add props

--- app/modules/promocard/scripts/MTPromoCard.jsx
+++ app/modules/promocard/scripts/MTPromoCard.jsx
@@ -1,10 +1,60 @@
 'use strict';
 
-import React, { Component, PropTypes }  from 'react';
+import React, { PropTypes }  from 'react';
+import {Pestle} from '@natgeo/mortar-pestle';
+import events from './events';
 
-class MTPromoCard extends Component {
-  render() {
-      return <h1>Promo Card</h1>
-  }
-}
+const promoData = {
+  // props to pass with the event
+};
+
+const promoClicked = () => {
+  Pestle.PubSub.publish(events.promoClicked, promoData);
+};
+
+const MTPromoCard = () => {
+  return(
+    <div className="mt2_promo-card-container">
+      <div onClick={promoClicked}>
+        <h1>I Render Well</h1>
+      </div>
+    </div>
+  );
+};
+
+MTPromoCard.PropTypes = {
+  image: PropTypes.shape({
+    url: PropTypes.string,
+    position: PropTypes.oneOf(['above', 'below', 'left', 'right']),
+    containerCSSClass: PropTypes.string,
+    inlineStyle: PropTypes.string
+  }),
+  text: PropTypes.shape({
+    containerCSSClass: PropTypes.string,
+    inlineStyle: PropTypes.string
+  }),
+  modal: PropTypes.bool,
+  target: PropTypes.oneOf(['_self', '_parent', '_blank', '_top']),
+  leadMedia: PropTypes.shape({
+    url: PropTypes.string,
+    aspectRatio: PropTypes.number,
+    height: PropTypes.number,
+    width: PropTypes.number,
+    internal: PropTypes.bool
+  }),
+  title: PropTypes.string,
+  hideTitle: PropTypes.bool,
+  dek: PropTypes.string,
+  hideDek: PropTypes.bool,
+  series: PropTypes.string,
+  hideSeries: PropTypes.bool,
+  byline: PropTypes.string,
+  hideByline: PropTypes.bool,
+  brandingBadgeLabel: PropTypes.string,
+  sponsorContent: PropTypes.bool,
+  sponsorContentLabel: PropTypes.string,
+  video: PropTypes.bool,
+  onClick: PropTypes.func
+};
+
 export default MTPromoCard;
