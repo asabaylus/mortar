085b1a9ec9fb30e25f909054d69baf1685ee67a3
Kekeli Sackey <hkekeli@users.noreply.github.com>
2016-07-29 11:09:30 -0400

    set classnames on node

--- app/modules/tooltips/scripts/MTTooltip.jsx
+++ app/modules/tooltips/scripts/MTTooltip.jsx
@@ -6,20 +6,6 @@ import classNames from 'classnames';
 
 class Tooltip extends Component {
   render() {
-    const tooltipClasses = classNames({
-      'mt2_tooltip': true,
-      'mt2_subh4': true,
-      'mt2_bgcolor--neutral--xxl': true
-    });
-
-    const tooltipBtnClasses = classNames({
-      'mt2_h5': true,
-      'mt2_color--neutral--xd': true,
-      'mt2_bordercolor--neutral--xl': true,
-      'mt2_bgcolor--neutral--xxxl': true,
-      'mt2_tooltip-btn': true
-    });
-
     let tooltipPlacement = classNames({
       'mt2_tooltip--top': this.props.top || this.props.placement === 'top',
       'mt2_tooltip--bottom': this.props.bottom || this.props.placement === 'bottom',
@@ -33,10 +19,10 @@ class Tooltip extends Component {
 
     return(
       <div className="mt2_tooltip-container">
-        <div className={`${tooltipClasses} ${tooltipPlacement}`}>
+        <div className={`${"mt2_tooltip mt2_subh4 mt2_bgcolor--neutral--xxl"} ${tooltipPlacement}`}>
           <p dangerouslySetInnerHTML={{__html: this.props.tooltipContent}} />
         </div>
-        <button className={tooltipBtnClasses}>?</button>
+        <button className="mt2_h5 mt2_color--neutral--xd mt2_bordercolor--neutral--xl mt2_bgcolor--neutral--xxxl mt2_tooltip-btn">?</button>
       </div>
     );
   }
