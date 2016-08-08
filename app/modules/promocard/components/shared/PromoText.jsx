cc2a52e45ca04520234291609f3f0aedc8bdff68
Ben Pearson <bpearson@groupcse.com>
2016-08-08 17:11:54 -0400

    [MORTAR-275] assign classes to text for color

--- app/modules/promocard/components/shared/PromoText.jsx
+++ app/modules/promocard/components/shared/PromoText.jsx
@@ -6,9 +6,9 @@ const PromoText = ({abstract, byline, kicker, title }) => {
   return(
     <div>
       {kicker ? <div className="mt2_color--neutral--l mt_subh2">{kicker}</div> : null}
-      {title ? <div className="mt2_h4">{title}</div> : null}
-      {abstract ? <div className="mt2_subh4">{abstract}</div> : null}
-      {byline ? <div className="mt2_h5">{byline}</div> : null}
+      {title ? <div className="mt2_color--neutral--xxd mt2_h4">{title}</div> : null}
+      {abstract ? <div className="mt2_color--neutral--xxd mt2_subh4">{abstract}</div> : null}
+      {byline ? <div className="mt2_color--neutral--xxd mt2_h5">{byline}</div> : null}
     </div>
   );
 };
