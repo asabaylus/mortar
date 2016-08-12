f1689cde70b24754db9526993474da29310eea35
Ben Pearson <bpearson@groupcse.com>
2016-08-12 14:44:28 -0400

    [MORTAR-275-2] change onClick function to assignment

--- app/modules/promocard/scripts/MTPromoCard.jsx
+++ app/modules/promocard/scripts/MTPromoCard.jsx
@@ -12,6 +12,7 @@ const MTPromoCard = (props) => {
   };
 
   const promoClicked = () => {
+    console.log("---------- Promo Clicked ---------");
     props.onClick();
     Pestle.PubSub.publish(events.promoClicked, promoData);
   };
@@ -36,7 +37,7 @@ const MTPromoCard = (props) => {
 
   let attrs = {
     className: "mt2_promocard-container",
-    onClick: promoClicked(),
+    onClick: promoClicked,
     href: generateHref(props.link.url, props.link.trackingCodes),
     target: props.link.target
   };
