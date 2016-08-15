def004376062160fbb47d26168d5f2c5d403af7a
Ben Pearson <bpearsoncse@users.noreply.github.com>
2016-08-15 10:19:32 -0400

    [feature/MORTAR-273-2] fix unit test errors (#202)

--- app/modules/promocard/configurator/MTPromoCardConfiguratorPestle.js
+++ app/modules/promocard/configurator/MTPromoCardConfiguratorPestle.js
@@ -14,37 +14,37 @@ class PromoCardConfigurator extends Module {
 
   init() {
     const props = {
-      id: "hero_promocard_0",
-      type: "article",
+      id: 'hero_promocard_0',
+      type: 'article',
       config: {
-        aspectRatio: "photo",
+        aspectRatio: 'photo',
         sponsored: true
       },
       link: {
-        url: "http://ngm.nationalgeographic.com/2007/05/zambia-wildlife/eckstrom-text",
-        target: "_blank",
+        url: 'http://ngm.nationalgeographic.com/2007/05/zambia-wildlife/eckstrom-text',
+        target: '_blank',
         trackingCodes: [
-          "utm_medium=website",
-          "utm_source=site"
+          'utm_medium=website',
+          'utm_source=site'
         ]
       },
       leadMedia: {
-        url: "http://placehold.it/800x600",
+        url: 'http://placehold.it/800x600',
         aspectRatio: 0.6667,
         height: 500,
         width: 350,
-        altText: "Picture of a caiman swimming underwater in Pantanal, Brazil",
-        srcset: ["http://placehold.it/400x300 400w", "http://placehold.it/800x600 800w", "http://placehold.it/1600x1200 1600w"],
-        containerCSSClass: "",
+        altText: 'Picture of a caiman swimming underwater in Pantanal, Brazil',
+        srcset: ['http://placehold.it/400x300 400w', 'http://placehold.it/800x600 800w', 'http://placehold.it/1600x1200 1600w'],
+        containerCSSClass: '',
         inlineStyle: null
       },
       text: {
-        title: "This is the title of the card",
-        dek: "This is a short dek for the card.",
-        kicker: "Kicker",
-        byline: "This is a great byline",
-        duration: "",
-        publishDate: "Thu Aug 04 13:30:39 EDT 2016"
+        title: 'This is the title of the card',
+        dek: 'This is a short dek for the card.',
+        kicker: 'Kicker',
+        byline: 'This is a great byline',
+        duration: '',
+        publishDate: 'Thu Aug 04 13:30:39 EDT 2016'
       },
       cta: {
         url: 'http://www.nationalgeographic.com',
@@ -54,8 +54,8 @@ class PromoCardConfigurator extends Module {
       },
       onClick: () => {},
       modal: false,
-      brandingBadgeLabel: "",
-      sponsorContentLabel: "This is the Sponsor Content Label"
+      brandingBadgeLabel: '',
+      sponsorContentLabel: 'This is the Sponsor Content Label'
     };
 
     this.instance = ReactDOM.render(<PromoCardConfiguratorComponent initialProps={props} />, this.el);
