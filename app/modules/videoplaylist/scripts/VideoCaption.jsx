cb6d1da7c9470e0c8d0db0ad2d4cfb01a0dfb76d
sebasrgl <sebastian.ramirez@globant.com>
2016-09-08 12:25:15 -0500

    [SYTO-798] Use described font family on mortar and use old color clases in order to stylish the component

--- app/modules/videoplaylist/scripts/VideoCaption.jsx
+++ app/modules/videoplaylist/scripts/VideoCaption.jsx
@@ -31,7 +31,7 @@ class VideoCaption extends Component {
 
   truncateAbstract() {
     $(this.refs.abstract).css({height: '6em'}).dotdotdot({
-      after: $('<a class="mt3_show-more-link mt3_color--gray-40" href="#">Read More</a>'),
+      after: $('<a class="mt3_show-more-link mt3_color--neutral--xxxl" href="#">Read More</a>'),
       callback: (isTruncated, original) => {
         const $abstractEl = $(this.refs.abstract);
         if (!isTruncated) {
@@ -50,13 +50,13 @@ class VideoCaption extends Component {
   render() {
     const {title, abstract} = this.props;
     return (
-      <div className="mt3_video-playlist--current-information mt3_bgcolor--gray-66 mt3_color--white">
-        <h3 ref="title" className="mt3_video-playlist--current-information__title">
+      <div className="mt3_video-playlist--current-information">
+        <h3 ref="title" className="mt3_video-playlist--current-information__title mt3_color--neutral--xxxl">
           <span itemProp='headline' dangerouslySetInnerHTML={{__html: title}} />
         </h3>
 
         <figcaption className="mt3_caption-container--indent mt3_caption-container--indent--gray">
-          <div ref="abstract"  className="mt3_caption-body mt3_video-playlist--current-information__description mt3_color--gray-40">
+          <div ref="abstract"  className="mt3_caption-body mt3_video-playlist--current-information__description">
             <span itemProp='description' dangerouslySetInnerHTML={{__html: abstract}} />
           </div>
         </figcaption>
