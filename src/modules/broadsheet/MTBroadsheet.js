f7c9c8a7e2349bc1335abda3c72cd1be86fbbd03
Welch Canavan <welch.canavan@gmail.com>
2016-09-26 15:17:48 -0400

    conform data structure to JSON object

--- src/modules/broadsheet/MTBroadsheet.js
+++ src/modules/broadsheet/MTBroadsheet.js
@@ -1,11 +1,21 @@
 'use strict';
 
-import React, { Component, PropTypes }  from 'react';
-import * as moment from 'moment';
-// import Captions from './components/Sidebar';
+import React, { PropTypes }  from 'react';
 
 class Broadsheet extends Component {
   render() {
+    const bodyNodes = this.props.mainBody.map((node, index) => {
+      if(node.type === "text") {
+        return (
+          <div key={index} dangerouslySetInnerHTML={{__html: node.text}} />
+        )
+      }
+
+      return (
+        <img key={index} src={node.imageUrl} />
+      )
+    });
+
     return(
       <div>
         <a href="http://nationalgeographic.com/magazine">National Geographic Magazine</a>
@@ -18,31 +28,31 @@ class Broadsheet extends Component {
               <p>Photography: {this.props.mainPhotographer}</p>
             </a>
             <figure>
-              <img src={this.props.heroImage} />
+              <img src={this.props.leadMediaImage} />
               <figcaption>
-                {this.props.heroCaption}
-                <p>Photograph By: {this.props.heroAttributon}</p>
+                {this.props.leadMediaCaption}
+                <div dangerouslySetInnerHTML={{__html: this.props.leadMediaCredit}} />
               </figcaption>
             </figure>
-
-            <div dangerouslySetInnerHTML={{__html: this.props.mainAbstract}} />
-
-            <aside>
-              <header>
-                <p>Subscribe National Geographic Magazine</p>
-                <img src={this.props.sidebarCoverImage} />
-              </header>
-              <p>Also in this issue</p>
-              <ul>
-              {this.props.sidebarItems.map((item, index) => (
-                <li key={index}>
-                <h2>{item.title}</h2>
-                <p>{item.description}</p>
-                </li>
-              ))}
-              </ul>
-            </aside>
           </header>
+
+          {bodyNodes}
+
+          <aside>
+            <header>
+              <p>Subscribe National Geographic Magazine</p>
+              <img src={this.props.coverImage} />
+            </header>
+            <p>Also in this issue</p>
+            <ul>
+            {this.props.subStories.map((item, index) => (
+              <li key={index}>
+              <h2>{item.text.title}</h2>
+              <p>{item.text.dek}</p>
+              </li>
+            ))}
+            </ul>
+          </aside>
         </section>
       </div>
     );
@@ -50,20 +60,19 @@ class Broadsheet extends Component {
 }
 
 Broadsheet.propTypes = {
-  heroAttributon: PropTypes.string,
-  heroCaption: PropTypes.string,
-  heroImage: PropTypes.string,
+  coverImage: PropTypes.string,
   issueDate: PropTypes.string,
   issueUrl: PropTypes.string,
+  leadMediaCaption: PropTypes.string,
+  leadMediaCredit: PropTypes.string,
+  leadMediaImage: PropTypes.string,
+  subStoriesHeading: PropTypes.string,
+  subStories: PropTypes.array,
   mainAuthor: PropTypes.string,
-  mainAbstract: PropTypes.string,
-  mainPhotographer: PropTypes.string,
+  mainBody: PropTypes.array,
+  mainPhotographer: PropTypes.array,
   mainTitle: PropTypes.string,
-  mainUrl: PropTypes.string,
-  sidebarItems: PropTypes.arrayOf(PropTypes.shape({
-    title: PropTypes.string,
-    description: PropTypes.string
-  }))
+  mainUrl: PropTypes.string
 }
 
 export default Broadsheet;
--- src/modules/broadsheet/MTBroadsheetPestle.js
+++ src/modules/broadsheet/MTBroadsheetPestle.js
@@ -8,18 +8,22 @@ import MTBroadsheet from './MTBroadsheet';
 class Broadsheet extends Module {
 
   init() {
+    const issue = this.options.endpoint.issue;
+
     ReactDOM.render(<MTBroadsheet
-      heroAttributon={this.options.heroAttributon}
-      heroCaption={this.options.heroCaption}
-      heroImage={this.options.heroImage}
-      issueDate={this.options.issueDate}
-      issueUrl={this.options.issueUrl}
-      mainAuthor={this.options.mainAuthor}
-      mainAbstract={this.options.mainAbstract}
-      mainPhotographer={this.options.mainPhotographer}
-      mainTitle={this.options.mainTitle}
-      mainUrl={this.options.mainUrl}
-      sidebarItems={this.options.sidebarItems}
+      coverImage={issue.subscribe.image.imageUrl}
+      issueUrl={issue.url}
+      issueDate={issue.text.kicker.label}
+      mainAuthor={issue.text.contributors.writers[0].name}
+      mainBody={issue.body}
+      mainPhotographer={issue.text.contributors.photographers[0].name}
+      mainTitle={issue.text.title}
+      mainUrl={issue.text.kicker.url}
+      leadMediaImage={issue.leadMedia[0].imageUrl}
+      leadMediaCaption={issue.leadMedia[0].caption}
+      leadMediaCredit={issue.leadMedia[0].credit}
+      subStoryHeading={issue.subStories.heading}
+      subStories={issue.subStories.stories}
     />, this.el);
   }
 }
