6b3b9fb78a068506541afe40b8e1e57fac39b3c5
Asa Baylus <asa@baylus.com>
2016-10-05 21:56:00 -0400

    [SYTO-928] - added border animation

--- src/modules/broadsheet/MTBroadsheet.js
+++ src/modules/broadsheet/MTBroadsheet.js
@@ -3,12 +3,15 @@
 import React, { Component, PropTypes }  from 'react';
 import PromoImage from '../promocard/components/shared/PromoImage';
 import Image from '@natgeo/modules-images';
+import _debounce from 'lodash/debounce';
+import _delay from 'lodash/delay';
 
 class Broadsheet extends Component {
 
   constructor(props) {
     super(props);
-    this.resizeHandler = null;
+    this.debouncedReveal = null;
+    this.reveal = this.reveal.bind(this);
     this.getWidth = this.getWidth.bind(this);
     this.calcAspectRatio = this.calcAspectRatio.bind(this);
     this.state = {
@@ -54,6 +57,45 @@ class Broadsheet extends Component {
     return (height - corner) / (width - corner);
   }
 
+  reveal() {
+    let el = this.refs.broadsheet;
+    let top = el.offsetTop;
+    let left = el.offsetLeft;
+    let width = el.offsetWidth;
+    let height = el.offsetHeight;
+
+    while(el.offsetParent) {
+      el = el.offsetParent;
+      top += el.offsetTop;
+      left += el.offsetLeft;
+    }
+
+    if (
+      top < (window.pageYOffset + window.innerHeight) &&
+      left < (window.pageXOffset + window.innerWidth) &&
+      (top + height) > window.pageYOffset &&
+      (left + width) > window.pageXOffset
+    ) {
+      console.log('broadsheet in viewport');
+      this.setState({growClass: "broadsheet_wrapper--grow"});
+    };
+  }
+
+  componentDidMount() {
+    // wait a few seconds because the user may need download the contents/images
+    // before we start the animation if the broadsheet is inframe on initial
+    // page load
+    _delay(this.reveal, 2000);
+    this.debouncedReveal = _debounce(this.reveal, 500);
+    window.addEventListener('resize', this.debouncedReveal);
+    window.addEventListener('scroll', this.debouncedReveal);
+  }
+
+  componentDidUnMount() {
+    window.removeEventListener('resize', this.debouncedReveal);
+    window.removeEventListener('scroll', this.debouncedReveal);
+  }
+
   render() {
     const bodyNodes = this.props.mainBody.map((node, index) => {
 
@@ -86,67 +128,70 @@ class Broadsheet extends Component {
     });
 
     return(
-      <div className="broadsheet mt3_bgcolor--primary">
-        <a className="mt3_color--black mt3_heading--h6--2" href='http://nationalgeographic.com/magazine'>National Geographic Magazine</a>
-        <section className="mt3_bgcolor--white">
-          <header className="mt3_row" style={{'minHeight': '450px'}}>
-            <div className="mt3_broadsheet-leadMedia-header">
-              <a className="mt3_broadsheet-leadMedia-issue-date mt3_color--white mt3_haas-heading" href={this.props.issueUrl}>{this.props.issueDate}</a>
-              <a className="mt3_broadsheet-leadMedia-title mt3_btn mt3_btn--naked" href={this.props.mainUrl}>
-                <h1 className="mt3_color--white mt3_haas-heading">{this.props.mainTitle}</h1>
-                <p>
-                  <span className="mt3_georgia-heading mt3_broadsheet-leadMedia-by-label">By:</span>
-                  <span className="mt3_georgia-heading mt3_broadsheet-leadMedia-by">{this.props.mainAuthor}</span>
-                </p>
-                <p>
-                  <span className="mt3_georgia-heading mt3_broadsheet-leadMedia-photograph-label">Photography:</span>
-                  <span className="mt3_georgia-heading mt3_broadsheet-leadMedia-photograph">{this.props.mainPhotographer}</span>
-                </p>
-              </a>
+      <div ref="broadsheet" className={"broadsheet_wrapper " + this.state.growClass} data-reveal="true">
+        <div className="broadsheet">
+          <a className="mt3_color--black mt3_heading--h6--2" href='http://nationalgeographic.com/magazine'>National Geographic Magazine</a>
+          <section className="mt3_bgcolor--white">
+            <header className="mt3_row" style={{'minHeight': '450px'}}>
+              <div className="mt3_broadsheet-leadMedia-header">
+                <a className="mt3_broadsheet-leadMedia-issue-date mt3_color--white mt3_haas-heading" href={this.props.issueUrl}>{this.props.issueDate}</a>
+                <a className="mt3_broadsheet-leadMedia-title mt3_btn mt3_btn--naked" href={this.props.mainUrl}>
+                  <h1 className="mt3_color--white mt3_haas-heading">{this.props.mainTitle}</h1>
+                  <p>
+                    <span className="mt3_georgia-heading mt3_broadsheet-leadMedia-by-label">By:</span>
+                    <span className="mt3_georgia-heading mt3_broadsheet-leadMedia-by">{this.props.mainAuthor}</span>
+                  </p>
+                  <p>
+                    <span className="mt3_georgia-heading mt3_broadsheet-leadMedia-photograph-label">Photography:</span>
+                    <span className="mt3_georgia-heading mt3_broadsheet-leadMedia-photograph">{this.props.mainPhotographer}</span>
+                  </p>
+                </a>
+              </div>
+              <div className="mt3_broadsheet-leadMedia-image mt3_promocard-gallery-images--image2-large">
+                <PromoImage
+                  type="image"
+                  config={''}
+                  fadeSpeed={0}
+                  leadMedia={this.props.leadMedia}
+                  childFrameAspectRatio={this.calcAspectRatio()}
+                  secondImage={false}
+                  breakpoint={this.state.breakpoint} />
+              </div>
+            </header>
+            <div className="mt3_row">
+              <div className="mt3_article mt3_col-8">
+              {bodyNodes}
+              </div>
+              <aside className="mt3_broadsheet-col-right mt3_col-4 mt3_col-gut">
+                <header className="mt3_broadsheet-cover-heading mt3_verlag-heading">
+                  <p><span className="mt3_broadsheet-subscribe-label">Subscribe</span><br/> National Geographic Magazine</p>
+                  <Image
+                    aspectRatio={this.props.coverImage.aspectRatio}
+                    frameAspectRatio={this.props.coverImage.aspectRatio}
+                    lazyLoad={false}
+                    placeholderBackgroundColor={"rgb(0,0,0)"}
+                    letterboxBackgroundColor = {"rgb(0,0,0)"}
+                    placeholder="none"
+                    height={this.props.height}
+                    width={this.props.width}
+                    altText="National Geographic Magazine Cover Image"
+                    src={this.props.coverImage.src}
+                    srcset={this.props.coverImage.srcset}
+                  />
+                </header>
+                <p className="mt3_broadsheet-story-also-in mt3_verlag-heading">Also in this issue</p>
+                <ul className="mt3_body">
+                {this.props.subStories.map((item, index) => (
+                  <li key={index}>
+                  <h2 className="mt3_broadsheet-story-title mt3_haas-heading mt3_color--black">{item.text.title}</h2>
+                  <p className="mt3_broadsheet-story-dek mt3_color--gray66">{item.text.dek}</p>
+                  </li>
+                ))}
+                </ul>
+              </aside>
             </div>
-            <div className="mt3_broadsheet-leadMedia-image mt3_promocard-gallery-images--image2-large">
-              <PromoImage
-                type="image"
-                config={''}
-                leadMedia={this.props.leadMedia}
-                childFrameAspectRatio={this.calcAspectRatio()}
-                secondImage={false}
-                breakpoint={this.state.breakpoint} />
-            </div>
-          </header>
-          <div className="mt3_row">
-            <div className="mt3_article mt3_col-8">
-            {bodyNodes}
-            </div>
-            <aside className="mt3_broadsheet-col-right mt3_col-4 mt3_col-gut">
-              <header className="mt3_broadsheet-cover-heading mt3_verlag-heading">
-                <p><span className="mt3_broadsheet-subscribe-label">Subscribe</span><br/> National Geographic Magazine</p>
-                <Image
-                  aspectRatio={this.props.coverImage.aspectRatio}
-                  frameAspectRatio={this.props.coverImage.aspectRatio}
-                  lazyLoad={false}
-                  placeholderBackgroundColor={"rgb(0,0,0)"}
-                  letterboxBackgroundColor = {"rgb(0,0,0)"}
-                  placeholder="none"
-                  height={this.props.height}
-                  width={this.props.width}
-                  altText="National Geographic Magazine Cover Image"
-                  src={this.props.coverImage.src}
-                  srcset={this.props.coverImage.srcset}
-                />
-              </header>
-              <p className="mt3_broadsheet-story-also-in mt3_verlag-heading">Also in this issue</p>
-              <ul className="mt3_body">
-              {this.props.subStories.map((item, index) => (
-                <li key={index}>
-                <h2 className="mt3_broadsheet-story-title mt3_haas-heading mt3_color--black">{item.text.title}</h2>
-                <p className="mt3_broadsheet-story-dek mt3_color--gray66">{item.text.dek}</p>
-                </li>
-              ))}
-              </ul>
-            </aside>
-          </div>
-        </section>
+          </section>
+        </div>
       </div>
     );
   }
