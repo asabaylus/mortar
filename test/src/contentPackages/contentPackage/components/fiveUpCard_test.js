ecb662f4a65909df2e847905eae5b5a29c3d143b
pgurley <pgurley@groupcse.com>
2016-12-05 15:11:10 -0500

    [AEM-6821] [AEM-6823] Adding in test for five up card, adding checks for new props being passed to five up promo (#468)

---
+++ test/src/contentPackages/contentPackage/components/fiveUpCard_test.js
@@ -0,0 +1,139 @@
+'use strict';
+
+import FiveUpCard from '../../../../../src/contentPackages/contentpackage/components/FiveUpCard';
+import mockModel from './testData';
+import {shallow} from 'enzyme';
+import React from 'react';
+
+
+describe('FiveUp Card', () => {
+
+  describe(':: Dark Theme ::Kicker', () => {
+    let el;
+    const options = mockModel;
+
+    before(() => {
+
+      el = shallow(<FiveUpCard cardNum={1} key={'fiveUp-promo-1'} {...options.dataModel.components[0].stories[0]} theme={'dark'} showImage={true} showKicker={true} />);
+    });
+
+    it('FiveUpCard wrapper should be rendered with expected class', () => {
+      expect(el.props().className).to.equal('mt3_fiveup-card');
+    });
+
+    it('The first element in the card should be a div wide link', () => {
+      expect(el.childAt(0).html()).to.equal('<a class="mt3_fiveup-divlink" href="/content/magazine/en_US/test-article-by-jani-actman.html?utm_medium=site&amp;utm_source=ng.com" target="_self"></a>');
+    });
+
+    it('There should be a kicker', () => {
+      expect(el.find('.mt3_kicker').length).to.equal(1);
+    });
+
+    it('The kicker should have the proper class for its theme', () => {
+      expect(el.find('.mt3_kicker').props().className).to.equal('mt3_color--white mt3_kicker');
+    });
+
+    it('The kicker link should have the proper href value', () => {
+      expect(el.find('.mt3_kicker').props().children.props.href).to.equal('/content/news/en_US/weird-wild.html?utm_medium=site&utm_source=ng.com');
+    });
+
+    it('The kicker link should have the proper title value', () => {
+      expect(el.find('.mt3_kicker').props().children.props.children).to.equal('Weird & Wild');
+    });
+
+    it('There should be a title', () => {
+      expect(el.find('.mt3_fiveup-card-title').length).to.equal(1);
+    });
+
+    it('The title should have the proper markup', () => {
+      expect(el.find('.mt3_fiveup-card-title').html()).to.equal('<div class="mt3_fiveup-card-title">Use this title instead</div>');
+    });
+
+    it('The wrapping div should indicate there is an image', () => {
+      expect(el.find('.mt3_fiveup-card-row--has-image').length).to.equal(1);
+    });
+
+    it('The proper img src should be passed to the component', () => {
+      expect(el.childAt(3).props().src).to.equal('/content/dam/photography/rights-exempt/PT_hero_42_153645159.jpg');
+    });
+
+
+    it('The proper frameAspectRatio should be passed to the component', () => {
+      expect(el.childAt(3).props().frameAspectRatio).to.equal('16:9');
+    });
+
+  });
+
+  describe(':: Light Theme', () => {
+    let el;
+    const options = mockModel;
+
+    before(() => {
+
+      el = shallow(<FiveUpCard cardNum={1} key={'fiveUp-promo-1'} {...options.dataModel.components[0].stories[0]} theme={'light'} showImage={true} showKicker={true} />);
+    });
+
+    it('The kicker should have the proper class for its theme', () => {
+      expect(el.find('.mt3_kicker').props().className).to.equal('mt3_color--gray40 mt3_kicker');
+    });
+
+  });
+
+  describe(':: Kicker :: No Link', () => {
+    let el;
+    const options = mockModel;
+
+    before(() => {
+
+      el = shallow(<FiveUpCard cardNum={1} key={'fiveUp-promo-1'} {...options.dataModel.components[0].stories[4]} theme={'light'} showImage={true} showKicker={false} />);
+    });
+    it('There should NOT be a link in the kicker', () => {
+      expect(el.find('.mt3_kicker').find('a').length).to.equal(0);
+    });
+
+    it('It should have the proper markup', () => {
+      expect(el.find('.mt3_kicker').html()).to.equal('<span class="mt3_color--gray40 mt3_kicker mt3_fiveup-no-kicker">Partner Content</span>');
+    });
+  });
+
+  describe(':: No Kicker', () => {
+    let el;
+    const options = mockModel;
+
+    before(() => {
+
+      el = shallow(<FiveUpCard cardNum={1} key={'fiveUp-promo-1'} {...options.dataModel.components[0].stories[0]} theme={'light'} showImage={true} showKicker={false} />);
+    });
+    it('There should NOT be a kicker', () => {
+      expect(el.find('.mt3_kicker').length).to.equal(0);
+    });
+  });
+
+  describe(':: Sponsored :: No Kicker', () => {
+    let el;
+    const options = mockModel;
+
+    before(() => {
+
+      el = shallow(<FiveUpCard cardNum={1} key={'fiveUp-promo-1'} {...options.dataModel.components[0].stories[1]} theme={'light'} showImage={true} showKicker={false} />);
+    });
+    it('There should be a "No Kicker" class', () => {
+      expect(el.find('.mt3_fiveup-no-kicker').length).to.equal(1);
+    });
+  });
+
+
+  describe(':: No Image', () => {
+    let el;
+    const options = mockModel;
+
+    before(() => {
+      el = shallow(<FiveUpCard cardNum={1} key={'fiveUp-promo-1'} {...options.dataModel.components[0].stories[0]} theme={'light'} showImage={false} showKicker={false} />);
+      debugger;
+    });
+
+    it('The wrapping div should NOT indicate there is an image', () => {
+      expect(el.find('.mt3_fiveup-card-row--has-image').length).to.equal(0);
+    });
+  });
+});
--- test/src/contentPackages/contentPackage/components/fiveUp_test.js
+++ test/src/contentPackages/contentPackage/components/fiveUp_test.js
@@ -6,46 +6,105 @@ import {shallow} from 'enzyme';
 import React from 'react';
 
 describe('FiveUp Component', () => {
-  let el;
-  const options = mockModel;
 
-  before(() => {
-    el = shallow(<FiveUpComponent
-        theme={options.theme}
-        model={options.dataModel.components[0]}
-    />);
+  describe(':: Default', () => {
+    let el;
+    const options = mockModel;
+
+      before(() => {
+        el = shallow(<FiveUpComponent
+            theme={options.theme}
+            model={options.dataModel.components[0]}
+        />);
+      });
+
+      it('FiveUp div should be rendered with expected class', () => {
+        expect(el.props().children.props.className).to.equal("mt3_fiveup");
+      });
+
+      it('Heading should be rendered and display provided text', () => {
+        expect(el.childAt(0).childAt(0).html()).to.equal(`<div class="mt3_left-and-right-package-header">Most Read</div>`);
+      });
+
+      it('Expect there to be 5 Card components', () => {
+        expect(el.find("FiveUpCard")).to.have.length(5);
+      });
+
+      it('First Card should have the proper type', () => {
+        const firstPromoCard = el.find("FiveUpCard").first();
+        expect(firstPromoCard.props().type).to.equal("article");
+      });
+
+      it('First Promo Card\'s Lead Media should have the proper aspect ratio', () => {
+        const firstPromoCard = el.find("FiveUpCard").first();
+        expect(firstPromoCard.props().leadMedia[0].aspectRatio).to.equal(0.48245614035088);
+      });
+
+      it('First Promo Card\'s config should have the proper aspect ratio', () => {
+        const firstPromoCard = el.find("FiveUpCard").first();
+        expect(firstPromoCard.props().config.aspectRatio).to.equal("16:9");
+      });
+
+      it('The Second Promo Card should have the proper title', () => {
+        const secondPromoCard = el.find("FiveUpCard").nodes[1];
+        expect(secondPromoCard.props.text.title).to.equal("Photo Gallery Test Ipsum");
+      });
   });
 
-  it('FiveUp div should be rendered with expected class', () => {
-    expect(el.props().children.props.className).to.equal("mt3_fiveup");
+
+  describe(':: No Numbers', () => {
+    let el;
+    const options = mockModel;
+    before(() => {
+      let testModel = options.dataModel.components[0];
+      testModel.showNumbers = false;
+
+      el = shallow(<FiveUpComponent
+          theme={options.theme}
+          model={testModel}
+      />);
+    });
+    it('FiveUp div should be rendered with expected class', () => {
+      expect(el.props().children.props.className).to.equal("mt3_fiveup mt3_fiveup--no-numbers");
+    });
   });
 
-  it('Heading should be rendered and display provided text', () => {
-    expect(el.childAt(0).childAt(0).html()).to.equal(`<div class="mt3_left-and-right-package-header">Most Read</div>`);
+  describe(':: No Kickers', () => {
+    let el;
+    const options = mockModel;
+
+    before(() => {
+      let testModel = options.dataModel.components[0];
+      testModel.showNumbers = true;
+      testModel.showKickers = false;
+
+      el = shallow(<FiveUpComponent
+          theme={options.theme}
+          model={testModel}
+      />);
+    });
+    it('FiveUp div should be rendered with expected class', () => {
+      expect(el.props().children.props.className).to.equal("mt3_fiveup mt3_fiveup--no-kicker");
+    });
   });
 
-  it('Expect there to be 5 Card components', () => {
-    expect(el.find("FiveUpCard")).to.have.length(5);
-  });
+  describe(':: No Numbers ::No Kickers', () => {
+    let el;
+    const options = mockModel;
 
-  it('First Card should have the proper type', () => {
-    const firstPromoCard = el.find("FiveUpCard").first();
-    expect(firstPromoCard.props().type).to.equal("article");
-  });
+    before(() => {
+      let testModel = options.dataModel.components[0];
+      testModel.showNumbers = false;
+      testModel.showKickers = false;
 
-  it('First Promo Card\'s Lead Media should have the proper aspect ratio', () => {
-    const firstPromoCard = el.find("FiveUpCard").first();
-    expect(firstPromoCard.props().leadMedia[0].aspectRatio).to.equal(0.48245614035088);
-  });
-
-  it('First Promo Card\'s config should have the proper aspect ratio', () => {
-    const firstPromoCard = el.find("FiveUpCard").first();
-    expect(firstPromoCard.props().config.aspectRatio).to.equal("16:9");
-  });
-
-  it('The Second Promo Card should have the proper title', () => {
-    const secondPromoCard = el.find("FiveUpCard").nodes[1];
-    expect(secondPromoCard.props.text.title).to.equal("Photo Gallery Test Ipsum");
+      el = shallow(<FiveUpComponent
+          theme={options.theme}
+          model={testModel}
+      />);
+    });
+    it('FiveUp div should be rendered with expected class', () => {
+      expect(el.props().children.props.className).to.equal("mt3_fiveup mt3_fiveup--no-kicker mt3_fiveup--no-numbers");
+    });
   });
 
 
